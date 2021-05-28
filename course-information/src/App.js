8f1d41ce97b781ac28b8f8eb70daefcb639ad56d
Kev Comedia <kevcomedia@gmail.com>
2021-05-28 15:09:40 +0800

    Refactor Content component

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -4,6 +4,22 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  )
+}
+
 const Total = (props) => {
   return <p>Number of exercises {props.total}</p>
 }
@@ -20,15 +36,14 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )

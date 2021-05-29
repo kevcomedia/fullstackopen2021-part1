a8e0e489451863db1c054c83e096563b85e612bd
Kev Comedia <kevcomedia@gmail.com>
2021-05-29 18:25:15 +0800

    Add ratings on button click

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,17 +6,29 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
       <h2>give feedback</h2>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
 
       <h2>statistics</h2>
-      <p>good 6</p>
-      <p>neutral 2</p>
-      <p>bad 1</p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }

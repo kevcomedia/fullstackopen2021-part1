124ccbd5ef32191f74d5f33724eb20525b1a9629
Kev Comedia <kevcomedia@gmail.com>
2021-05-29 18:28:48 +0800

    Add all statistic

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -7,6 +7,7 @@ const Statistics = ({ good, neutral, bad }) => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
     </>
   )
 }

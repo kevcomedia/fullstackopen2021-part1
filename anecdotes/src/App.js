9d5b5fb2b4628491ad0e3f15b4481faad2799105
Kev Comedia <kevcomedia@gmail.com>
2021-05-30 16:23:35 +0800

    Add button to show random anecdote

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -15,7 +15,16 @@ const App = () => {
 
   const [selected, setSelected] = useState(0)
 
-  return <div>{anecdotes[selected]}</div>
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleClick}>next anecdote</button>
+    </div>
+  )
 }
 
 export default App
